4abe7c7f1b1eefa3c8bbb91dfef1984afcbd2886
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-21 21:47:33 +0300

    first commit

--- SolarSystemARKit/ViewController.swift
+++ SolarSystemARKit/ViewController.swift
@@ -18,15 +18,36 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+
+        // let myBox = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        // let mySphere = createSphere(radius: 0.1, content: "sea.png", vector: SCNVector3(0, 0.2, -1))
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let world = createSphere(radius: 0.1, content: "Savannah.png", vector: SCNVector3(0, 0.2, -1))
+        let mars = createSphere(radius: 0.2, content: "Martian.png", vector: SCNVector3(0.5, 0.2, -1))
+        let venus = createSphere(radius: 0.15, content: "Venusian.png", vector: SCNVector3(1, 0.2, -1))
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene.rootNode.addChildNode(world)
+        sceneView.scene.rootNode.addChildNode(mars)
+        sceneView.scene.rootNode.addChildNode(venus)
+
+        sceneView.automaticallyUpdatesLighting = true
+        
+    }
+    
+    func createSphere(radius: CGFloat, content: String, vector: SCNVector3) -> SCNNode{
+        let mySphere = SCNSphere(radius: radius)
+        let boxMaterial = SCNMaterial()
+        
+        boxMaterial.diffuse.contents = UIImage(named: "art.scnassets/\(content)")
+        mySphere.materials = [boxMaterial]
+        
+        let node = SCNNode()
+        
+        node.position = vector
+        node.geometry = mySphere
+        
+        return node
     }
     
     override func viewWillAppear(_ animated: Bool) {
